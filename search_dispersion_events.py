5822d01c450d47c52c1f4b64176536c9717c5b09
Daniel da Silva <mail@danieldasilva.org>
2020-09-22 21:46:33 -0400

    Got to a stopping point in developing the physical algorithm where it produces strong results and I think it's ready to start developing the code to create a training set for a CNN. The algorithm tracks the change in Eic, but only counts contributions if the physical parameters at the current timestep are in range. I introduced physical restrictions on the  density (I integrate the number flux) and the flux at the Eic energy. I spent a while iterating on the thresholds and arrived at a decent set. Right now, it is very effective at finding the examples from the example set you/Li-jen sent, and the false positives are pretty low.

--- search_dispersion_events.py
+++ search_dispersion_events.py
@@ -17,11 +17,15 @@ import sys
 
 DEFAULT_INTERVAL_LENGTH = 60
 
-DEFAULT_MAX_FLUX_THRESHOLD = 6.0  # eV
+DEFAULT_DENSITY_LOG_THRESHOLD = 7.25  # eV
 
-DEFAULT_INTEGRAL_THRESHOLD = 0.7  # Log eV
+#DEFAULT_INTEGRAL_THRESHOLD = 0.7  # Log eV
+DEFAULT_INTEGRAL_THRESHOLD = 0.6  # Log eV
 
-DEFAULT_FRAC_ABOVE_ZERO_THRESHOLD = .7
+DEFAULT_MIN_MLAT = 55
+
+DEFAULT_FLUX_AT_EIC_THRESHOLD = 10**5.5
+DEFAULT_UPPER_AREA_FRAC_THRESHOLD = 0.8
 
 
 def main():
@@ -32,17 +36,6 @@ def main():
     parser.add_argument('-I', '--interval-length', default=DEFAULT_INTERVAL_LENGTH,
                         type=float,
                         help='Interval length (seconds)')
-    parser.add_argument('-F', '--max-flux-threshold', default=DEFAULT_MAX_FLUX_THRESHOLD,
-                        type=float,
-                        help='Max Flux Threshold')
-    parser.add_argument('-S', '--integral-threshold', default=DEFAULT_INTEGRAL_THRESHOLD,
-                        type=float,
-                        help='Integral threshold')
-    parser.add_argument('-f', '--frac-above-zero-threshold',
-                        default=DEFAULT_FRAC_ABOVE_ZERO_THRESHOLD,
-                        type=float,
-                        help='Fraction above zero threshold')
-
     args = parser.parse_args()
 
     # Check file exists
@@ -53,11 +46,10 @@ def main():
     # Calculate  smoothed dEic/dt in log-space
     fh = read_file(args.dmsp_file)
 
-    dEicdt_smooth = estimate_log_Eic_smooth_derivative(fh)
+    dEicdt_smooth, Eic_smooth = estimate_log_Eic_smooth_derivative(fh)
 
     df_match = walk_and_integrate(
-        fh, dEicdt_smooth, args.interval_length, args.max_flux_threshold,
-        args.integral_threshold, args.frac_above_zero_threshold
+        fh, dEicdt_smooth, Eic_smooth, args.interval_length
     )
 
     # Output intervals to terminal
@@ -86,6 +78,8 @@ def read_file(filename):
     fh['mlt'] = hdf['Data']['Array Layout']['1D Parameters']['mlt'][:]
     fh['ion_d_flux'] = hdf['Data']['Array Layout']['2D Parameters']['ion_d_flux'][:]
     fh['ion_d_ener'] = hdf['Data']['Array Layout']['2D Parameters']['ion_d_ener'][:]
+    fh['density'] = 4 * np.pi * np.trapz(fh['ion_d_flux'].T, fh['ch_energy'], axis=1)
+    fh['peak_flux'] = np.max(fh['ion_d_ener'], axis=0)
 
     # Close file
     hdf.close()
@@ -150,20 +144,29 @@ def find_moving_average(a, window_size) :
 
 
 def estimate_log_Eic_smooth_derivative(fh, eic_window_size=11,
-                                       eic_deriv_window_size=5,
-                                       return_Eic=False):
+                                       eic_deriv_window_size=5):
     """Calculate the smoothed derivative of the smoothed Log10(Eic) parameter.
     
     Args
       fh: file handle returned by read_file()
     Returns
       dEicdt_smooth: smoothed derivative corresponding to times found in fh['t']
+      Eic_smooth: smoothed Eic cooresponding to times found in fh['t']
     """
     # Calcualte the Eic parameter. Throughout this function, the Eic is in
     # log-space.
     Eic = np.log10(estimate_Eic(fh, i=0, j=fh['t'].size))
     Eic_smooth = find_moving_average(Eic, eic_window_size)
 
+    en_inds = np.log10(fh['ch_energy']).searchsorted(Eic_smooth)
+    en_inds = [min(i, 18) for i in en_inds]
+    flux_at_Eic = fh['ion_d_ener'][en_inds, np.arange(Eic.size)]
+    flux_mask = (flux_at_Eic > DEFAULT_FLUX_AT_EIC_THRESHOLD)
+    
+    for i, cur_is_above in enumerate(flux_mask):
+        if not cur_is_above:
+            Eic_smooth[i] = np.nan
+    
     # Find the smoothed derivative of the smoothed log10(Eic) function. For sake
     # of simplicity, the derivative is estimated with a forward difference.
     dEic = Eic_smooth.copy()
@@ -175,14 +178,13 @@ def estimate_log_Eic_smooth_derivative(fh, eic_window_size=11,
     
     dEicdt_smooth = find_moving_average(dEic/dt, eic_deriv_window_size)
 
-    if return_Eic:
-        return dEicdt_smooth, Eic_smooth
-    else:
-        return dEicdt_smooth
+    dEicdt_smooth[np.isnan(dEicdt_smooth)] = 0
+    
+    
+    return dEicdt_smooth, Eic_smooth
 
 
-def walk_and_integrate(fh, dEicdt_smooth, interval_length, max_flux_threshold,
-                       integral_threshold, frac_above_zero_threshold,
+def walk_and_integrate(fh, dEicdt_smooth, Eic_smooth, interval_length, 
                        return_integrand=False):
     """Walk through windows in the file and test for matching intervals with
     integration of the metric function.
@@ -190,22 +192,17 @@ def walk_and_integrate(fh, dEicdt_smooth, interval_length, max_flux_threshold,
     Args
       fh: file handle returned by read_file()
       dEicdt_smooth: smoothed derivative corresponding to times found in fh['t']
+      Eic_smooth: smoothed Eic cooresponding to times found in fh['t']
       interval_length: length of interval
       return_integrand: return array of integrand values
-
-      == thresholds ==
-      max_flux_threshold: threshold for maximum threshold condition of algorithm
-      integral_threshold: threshold for accept/reject condition with integral
-          value
-      frac_above_zero_treshold: threshold for accept/reject condition with 
-          fraction of ingrand values above zero
     """
     # Make interval length into timedelta
     interval_length = timedelta(seconds=interval_length)
     bar = progressbar.ProgressBar()
     matching_intervals = IntervalTree()
-    integrals = []
     integrand_save = np.zeros(fh['t'].size)
+    integral_save = np.nan * np.zeros(fh['t'].size)
+    upper_area_frac_save = np.nan * integrand_save.copy() 
     
     # Walk through timeseries 
     for start_time_idx, start_time in bar(list(enumerate(fh['t']))):
@@ -222,7 +219,7 @@ def walk_and_integrate(fh, dEicdt_smooth, interval_length, max_flux_threshold,
         # |mlat| > 60 deg.
         mlat = fh['mlat'][start_time_idx:end_time_idx]
 
-        if not np.all(np.abs(mlat) > 60):
+        if not np.all(np.abs(mlat) > DEFAULT_MIN_MLAT):
             continue
         
         # Setup integrand for integration. This contains multiple
@@ -231,20 +228,26 @@ def walk_and_integrate(fh, dEicdt_smooth, interval_length, max_flux_threshold,
 
         ion_d_ener = fh['ion_d_ener'][:, start_time_idx:end_time_idx]
         ion_d_flux = fh['ion_d_flux'][:, start_time_idx:end_time_idx]
-
-        num_dens = integrate_number_density(ion_d_flux, fh['ch_energy'])
         
         with np.errstate(divide='ignore'):
-            max_flux = np.log10(ion_d_ener).max(axis=0)
-        max_flux_mask = (max_flux > max_flux_threshold).astype(int)        
-
-        #num_dens_mask = (num_dens > 1e10).astype(int)
-        num_dens_mask = np.ones_like(num_dens)
+            density_log = np.log10(fh['density'][start_time_idx:end_time_idx] + .01)
+        
+        density_mask = (density_log > DEFAULT_DENSITY_LOG_THRESHOLD).astype(int)        
+        
+        en_inds = np.log10(fh['ch_energy']).searchsorted(Eic_smooth[start_time_idx:end_time_idx])
+        en_inds = [min(i, 18) for i in en_inds]
+        flux_at_Eic = fh['ion_d_ener'][en_inds, np.arange(start_time_idx, end_time_idx)]
+        flux_at_Eic[np.isnan(Eic_smooth[start_time_idx:end_time_idx])] = np.nan
+        
+        with np.errstate(invalid='ignore'):
+            flux_at_Eic_mask = (
+                np.isfinite(flux_at_Eic) & (flux_at_Eic > DEFAULT_FLUX_AT_EIC_THRESHOLD)
+            ).astype(int)
         
         integrand = (
             mlat_direction *
-            max_flux_mask[:-1] *
-            num_dens_mask[:-1] *
+            density_mask[:-1] *
+            flux_at_Eic_mask[:-1] * 
             dEicdt_smooth[start_time_idx:end_time_idx-1]
         )
 
@@ -252,31 +255,40 @@ def walk_and_integrate(fh, dEicdt_smooth, interval_length, max_flux_threshold,
         dt = [delta.total_seconds() for delta in np.diff(t)]
 
         if integrand.size > 0:
-            integrand_save[start_time_idx:end_time_idx-1] = integrand
+            integrand_save[start_time_idx] = integrand[0]
         else:
             continue
         
         # Compute integral of the integrand over increments of dt using
         # rectangular integraion
         t = fh['t'][start_time_idx:end_time_idx]
-        dt = [delta.total_seconds() for delta in np.diff(t)]
+        dt = np.array([delta.total_seconds() for delta in np.diff(t)])
 
         integral = np.sum(integrand * dt)
+                
+        upper_integral = np.sum(integrand[integrand > 0] * dt[integrand > 0])
+        abs_integral =  np.sum(np.abs(integrand) * dt)
+        
+        if abs_integral > 0:
+            upper_area_frac = upper_integral / abs_integral
+        else:
+            upper_area_frac = 0
+        
+        upper_area_frac_save[start_time_idx] = upper_area_frac
+        integral_save[start_time_idx] = integral
         
         # Collect metadata used supplemented with interval if the interval is
         # accepted
-        frac_above_zero = np.sum(integrand > 0) / integrand.size
         integrand_min = np.min(integrand)
         integrand_mean = np.mean(integrand)
         integrand_max = np.max(integrand)
         
         # The test/accept condition on the integral value and the fraction of
         # values above zero.
-        if (integral > integral_threshold and
-            frac_above_zero > frac_above_zero_threshold):
+        #print(start_time, integrand_save[start_time_idx:end_time_idx-1].sum(), integrand.sum(), upper_area_frac)
+        if (integral > DEFAULT_INTEGRAL_THRESHOLD and upper_area_frac > DEFAULT_UPPER_AREA_FRAC_THRESHOLD):
             matching_intervals[start_time:end_time] = {
                 'integral': integral,
-                'frac_above_zero': frac_above_zero,
                 'integrand_min': integrand_min,
                 'integrand_mean': integrand_mean,
                 'integrand_max': integrand_max,
@@ -306,9 +318,6 @@ def walk_and_integrate(fh, dEicdt_smooth, interval_length, max_flux_threshold,
             min(interval.data['integral']),
             np.mean(list(interval.data['integral'])),
             max(interval.data['integral']),
-            min(interval.data['frac_above_zero']),
-            np.mean(list(interval.data['frac_above_zero'])),
-            max(interval.data['frac_above_zero']),
             min(interval.data['integrand_min']),
             np.mean(list(interval.data['integrand_mean'])),
             max(interval.data['integrand_max']),
@@ -317,31 +326,13 @@ def walk_and_integrate(fh, dEicdt_smooth, interval_length, max_flux_threshold,
     df_match = pd.DataFrame(df_match_rows, columns=[
         'start_time', 'end_time',
         'integral_min', 'integral_mean', 'integral_max',
-        'frac_above_zero_min', 'frac_above_zero_mean', 'frac_above_zero_max',
         'integrand_min', 'integrand_mean', 'integrand_max',
     ])
 
     if return_integrand:
-        return df_match, integrand_save
+        return df_match, integrand_save, integral_save, upper_area_frac_save
     else:
         return df_match
-
-
-def integrate_number_density(ion_d_flux, ch_energy):
-    """Integrates the ion_d_flux flux (number flux) over energy using
-    trapezoidal integration.
-
-    Args
-      ion_d_flux: number flux per energy
-      ch_energy: channel energy
-    Returns
-      number density
-    """
-    y = ion_d_flux.copy()
-    x = np.transpose([ch_energy]*y.shape[1])
-    num_dens = np.trapz(y, x, axis=0)
-
-    return num_dens
 
 
 if __name__ == '__main__':
