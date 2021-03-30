324cdb7998fbbca210b91b5d2bbaa0bfb772e4c3
Daniel da Silva <Daniel.e.daSilva@nasa.gov>
2021-03-30 10:21:03 -0400

    Remove full path to globalDownload.py; use version in path

--- download_dmsp.sh
+++ download_dmsp.sh
@@ -5,11 +5,10 @@
 
 # Edit to specify year. Edit the --start-date and --end-date below
 # to further adjust the range.
-yyyy=2010
+yyyy=2020
 
-for i in $(seq 16 19); do
-    
-    /home/daniel/anaconda3/bin/globalDownload.py --verbose --url=http://cedar.openmadrigal.org --outputDir=./data/validate/$yyyy/Satellite_F${i} --user_fullname='Daniel+da+Silva' --user_email=daniel.e.dasilva@nasa.gov --user_affiliation='NASA' --format='hdf5' --startDate="1/1/$yyyy" --endDate="12/31/$yyyy" --inst=8100 --kindat=102${i} &
+for i in $(seq 16 19); do   
+    globalDownload.py --verbose --url=http://cedar.openmadrigal.org --outputDir=./data/validate/$yyyy/Satellite_F${i} --user_fullname='Daniel+da+Silva' --user_email=daniel.e.dasilva@nasa.gov --user_affiliation='NASA' --format='hdf5' --startDate="1/1/$yyyy" --endDate="1/31/$yyyy" --inst=8100 --kindat=102${i} &
 done
 
 wait
