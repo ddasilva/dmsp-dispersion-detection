2b8844b6f4938a72a4a336091295e75dc851fbb6
Daniel da Silva <Daniel.e.daSilva@nasa.gov>
2021-06-03 18:45:33 -0400

    Code at time of paper submission

--- lib_lockwood1992.py
+++ lib_lockwood1992.py
@@ -11,7 +11,6 @@ from matplotlib.dates import date2num
 import numpy as np
 import pytz
 
-from lib_util import find_moving_average
 
 # Length of path the particle precipation travels down along the cusp field
 # line. In Lockwood 1992, it is suggested the true length is between 15-30 Re.
@@ -19,13 +18,13 @@ from lib_util import find_moving_average
 PRECIP_TRAVEL_PATHS = np.array([10., 20., 30.])
 
 
-def estimate_reconn_rate(dmsp_flux_fh, Eic_smooth, i=None, j=None):
+def estimate_reconn_rate(dmsp_flux_fh, Eic, i=None, j=None):
     """Estimate reconnection rate using in-situ ionospheric measurements and
     solar wind data. 
 
     Args
       dmsp_flux_fh: file handle (as returned by read_dmsp_flux_file)
-      Eic_smooth: smoothed Eic cooresponding to times found in dmsp_flux_fh['t'].
+      Eic: Eic cooresponding to times found in dmsp_flux_fh['t'].
         Note: In units of Log10(eV)
     Returns
       Ey_final: Estimated reconnection rate, in units of mV/m.
@@ -33,7 +32,7 @@ def estimate_reconn_rate(dmsp_flux_fh, Eic_smooth, i=None, j=None):
     # Calculate required parameters. Interpolate everything to the time axis
     # of Eic.
     # ------------------------------------------------------------------------
-    Eic = (10**Eic_smooth) * units.eV    
+    Eic = (10**Eic) * units.eV    
     t = dmsp_flux_fh['t']
     
     if i is not None and j is not None:
@@ -49,7 +48,7 @@ def estimate_reconn_rate(dmsp_flux_fh, Eic_smooth, i=None, j=None):
     dt.append(dt[-1])
     dt = np.array(dt) * units.s
     
-    dEicdt = find_moving_average(dEic/dt, 5)
+    dEicdt = dEic/dt
     
     # Magnetc field at MP
     Bmp = 50 * units.nT
@@ -73,7 +72,7 @@ def estimate_reconn_rate(dmsp_flux_fh, Eic_smooth, i=None, j=None):
         Ey = (
             (Bi * Vs * np.cos(alpha))
             /
-            (1 + (d/2) * np.sqrt(m/2) * (Eic**(-3/2)) * np.abs(dEicdt))
+            (1 + (d/2) * np.sqrt(m/2) * Eic**(-3/2) * np.abs(dEicdt))
         )
         dy = np.sqrt(Bi / Bmp)
         Ey_final = Ey / dy       
